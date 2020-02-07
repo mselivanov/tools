cf954d91556f29bdb6d739194a733273a785d4b1
Maksim Selivanau <maksim.selivanau@cantire.com>
2020-02-07 12:35:30 +0300

    Update tools
    - Add snippets folder
    - Add bash snippet for finding and renaming files

---
+++ scripts/snippets/bash/snippets.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Read file content into var
+# Watch file newlines - should be unix style
+export FILE_CONTENT="$(< /path_to_file)"
---
+++ snippets/bash/find_and_rename.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script searches for file matching particular file extension.
+# Renames them by adding postfix before extension.
+# Example: db_schema.json => db_schema_newpostfix.json
+
+# Start search in the current directory and search only in it (-maxdepth 1)
+# -regextype posix-extended: extended regular expressions, egrep
+# -regex '.*\.txt|.*\.json': find files ending with txt or json
+# -exec bash -c: execute bash command for all found files
+# -exec command {} +: this form of exec appends all found files in one line
+# None: name of the bash script; assigned to $0 parameter
+# $@: array of all the parameters  (file names)
+# ${f#./}: parameter substitution, removes minimum occurence of the pattern in the fron end of a variable; removes leading ./ from the filename
+# ${f%.*}: parameter substitution, remove the shortest part matching the pattern from the backend of a variable; .(dot) and everything else; in param expansion .(dot)
+# doesn't match any symbol
+# ${f##*.}: parameter substitution, remove the longest part matching the pattern from the front end of a variable; any symbol till the last dot 
+
+find . -maxdepth 1 -type f -regextype posix-extended -regex '.*\.txt|.*\.json' -exec bash -c 'for f in "$@"; do f=${f#./}; mv "$f" "./${f%.*}_newpostfix.${f##*.}"; done' None {} +

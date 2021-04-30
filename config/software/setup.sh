db881e6cdc11ed5bd75b7f817fa21b68518b8f83
Maksim Selivanau <maksim_selivanau@epam.com>
2021-04-30 19:56:34 +0300

    Add setup file for installing software with choco

---
+++ config/software/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Section: development
+choco install dbeaver -y
+
+# Section: productivity
+choco install freeplane -y
+choco install obsidian -y
+
+choco install calibre -y
